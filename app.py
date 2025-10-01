9a186cb982477155e7025d8bccb432fdd186d3f1
s57yao <141304067+s57yao@users.noreply.github.com>
2025-09-30 21:11:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True, port=8002)  
+    app.run(host='0.0.0.0',debug=False, port=8002)  
